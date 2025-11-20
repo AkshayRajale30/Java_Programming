b3d85b6f3d44784603d54a5700438ee4ec15d3c7
Akshay Ashok Rajale <akshayrajale1@gmail.com>
2025-11-20 23:27:09 +0530

    Add files via upload

--- Classworks/Day_04/program151.java
+++ Classworks/Day_04/program151.java
@@ -1,1 +1,17 @@
+class Pattern
+{
+    public void Display()
+    {
+        System.out.println("Inside Display");
+        // LOgic of pattern printing
+    }
+}
 
+class program151
+{
+    public static void main(String A[])
+    {
+        Pattern pobj = new Pattern();
+        pobj.Display();
+    }
+}
---
+++ Classworks/Day_04/program162.java
@@ -0,0 +1,49 @@
+// INPUT : 6
+// OUTPUT : 1  *  2  *  3  *  4
+
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iNo)
+    {
+        int iCnt = 0;
+        int iCount = 0;
+
+        iCount = 1;
+        for(iCnt = 1; iCnt <= iNo; iCnt++)
+        {
+            if(iCnt % 2 == 0)
+            {
+                System.out.print("*\t");
+            }
+            else
+            {
+                System.out.print(iCount+"\t");
+                iCount++;
+            }    
+        }
+        
+        System.out.println();
+    }
+}
+
+class program162
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+
+        System.out.println("Enter the frequency : ");
+        iValue = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue);
+
+        
+    }
+} 
+
